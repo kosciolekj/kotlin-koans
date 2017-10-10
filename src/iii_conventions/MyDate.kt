dbfe4200e2c566b8fda9d89cafe8ed74e75dcedc
Jacek Kościółek <jacek.kosciolek@mobica.com>
2017-10-10 15:46:53 +0200

    iii_conventions progress

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,20 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return when {
+            year != other.year -> return year - other.year
+            month != other.month -> return month - other.month
+            else -> dayOfMonth - other.dayOfMonth
+        }
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange {
+    return DateRange(this, other)
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +22,23 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+
+
+
+    operator fun contains(d: MyDate): Boolean {
+        val dCal: Calendar = Calendar.getInstance()
+        dCal.set(d.year, d.month, d.dayOfMonth)
+
+        val startCal: Calendar = Calendar.getInstance()
+        startCal.set(start.year, start.month, start.dayOfMonth)
+
+        val endCal: Calendar = Calendar.getInstance()
+        endCal.set(endInclusive.year, endInclusive.month, endInclusive.dayOfMonth)
+
+        return dCal.timeInMillis >= startCal.timeInMillis && dCal.timeInMillis <= endCal.timeInMillis
+    }
+
+
+}
+
